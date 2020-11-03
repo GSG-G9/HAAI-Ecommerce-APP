fff1804cf748b7741f06dbe2a901881d120b4265
hassanelnajjar <hassansamielnajjar@gmail.com>
2020-11-03 17:41:44 +0200

    Revert "go to branch"
    This reverts commit 7924b1480a8b84cfb254296b2694037174fdfd59.

--- js/logic.js
+++
@@ -1,6 +0,0 @@
-// Seller - Add new product
-function addProduct(product, products) {
-	return [...products, product];
-}
-
-module.exports = { addProduct };
--- js/test.js
+++ js/test.js
@@ -1,23 +1,3 @@
-const { addProduct } = require("./logic");
-
 test("check jest is working", () => {
 	expect(1).toBe(1);
-});
-
-describe("Test addProduct Function", () => {
-	test("should add input item - first parameter- to array -second parameter", () => {
-		const products = [{ id: 1 }, { id: 2 }, { id: 3 }];
-		const oneProduct = { id: 4 };
-		const actual = addProduct(oneProduct, products);
-		const expected = [...products, oneProduct];
-		expect(actual).toEqual(expected);
-	});
-
-	test("should add input item - first parameter- to array -second parameter even the array is empty", () => {
-		const products = [];
-		const oneProduct = { id: 1 };
-		const actual = addProduct(oneProduct, products);
-		const expected = [...products, oneProduct];
-		expect(actual).toEqual(expected);
-	});
 });
