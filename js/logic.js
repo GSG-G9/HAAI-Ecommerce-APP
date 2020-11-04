f6fabab06f1a55a1a2e5438e0ff74268489508f8
imansedky5 <imans.ewaiti@gmail.com>
2020-11-04 08:54:03 +0200

    added total price and delete logic pure functions relates #11 relates #8

--- js/logic.js
+++ js/logic.js
@@ -2,6 +2,17 @@
 function addProduct(product, products) {
 	return [...products, product];
 }
+//buyer - total price function /////////////////iman
+function totalPrice(cartProducts){
+	return cartProducts.map(item => item.price).reduce((acc, val)=> acc + val , 0)
+}
+//buyer - delete product /////////////iman
+function deleteProduct(cartProducts, choosenProduct){
+	return cartProducts.filter(item => item.id !== choosenProduct.id);
+
+}
+
+
 
 //Export functions
-module.exports = { addProduct };
+module.exports = { addProduct, totalPrice, deleteProduct};
